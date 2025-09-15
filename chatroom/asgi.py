22a14cb800d65fe62270d1d69f8944bead8611e9
bobz <bzhang5460@gmail.com>
2025-09-14 20:48:19 -0400

    e

--- chatroom/asgi.py
+++ chatroom/asgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from chat import consumers
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
+
 from chat.routing import websocket_urlpatterns
 django_asgi_app = get_asgi_application()
 
